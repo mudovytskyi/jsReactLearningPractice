b308ed4f8068af6a1a8f689b0f3b81e27c65c049
mudovytskyi <mudovytskyi@gmail.com>
2017-12-18 10:43:11 +0200

    Add api to Fetch a List of Names

--- api/index.js
+++ api/index.js
@@ -55,6 +55,20 @@ router.get('/contests', (req, res) => {
         });
 });
 
+router.get('/names/:nameIds', (req, res) => {
+    const nameIds = req.params.nameIds.split(',').map(Number);
+    let names = {};
+    mdb.collection('names').find({ id: {$in: nameIds }})
+        .each((err, name) => {
+            assert.equal(null, err);
+            if (!name) { // no more names
+                res.send({names});
+                return;
+            }
+            names[name.id] = name;
+        });
+});
+
 router.get('/contests/:contestId', (req, res) => {
    /*  console.log(`ID: ${req.url}`);
     //req.params.contestId
--- src/components/Contest.js
+++ src/components/Contest.js
@@ -6,12 +6,38 @@ class Contest extends Component {
     render() {
         return (
             <div className="Contest">
-                <div className="contest-description">
+                {/* <div className="contest-description">
                     {this.props.id}<br/>
                     {this.props.categoryName}<br/>
                     {this.props.contestName}<br/>
                     {this.props.description}<br/>
+                </div> */}
+                <div className="panel panel-default">
+                    <div className="panel-heading">
+                        <h3 className="panel-title">Contest Description</h3>
+                    </div>
+                    <div className="panel-body">
+                        <div className="contest-description">
+                            {this.props.id}<br/>
+                            {this.props.categoryName}<br/>
+                            {this.props.contestName}<br/>
+                            {this.props.description}<br/>
+                        </div>
+                    </div>
                 </div>
+
+                <div className="panel panel-default">
+                    <div className="panel-heading">
+                        <h3 className="panel-title">Proposed Names</h3>
+                    </div>
+                    <div className="panel-body">
+                        <ul className="list-group">
+                            <li className="list-group-item">Name one ...</li>
+                            <li className="list-group-item">Name two ...</li>
+                        </ul>
+                    </div>
+                </div>
+                
                 <div className="home-link link"
                     onClick={this.props.contestListClick}>
                     Contest List
